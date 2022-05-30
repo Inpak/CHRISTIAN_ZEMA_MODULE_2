1d00af73daccb3b65d1fc57939a84f28d69a86b5
Christian Zema <chriszeymar@gmail.com>
2022-05-30 13:35:05 +0000

    Upload New File

---
+++ Program_1.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = 'Christian Zema';
+  var favorite = 'FNB_APP';
+  var city = 'Vaal';
+  print('My name is $name');
+  print('My favorite application is $favorite');
+  print('I live  in $city');
+
+  
+}
